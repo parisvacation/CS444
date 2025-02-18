587f052e34b503cc7d76813b2f4a5fa4aa681100
ruxid2 <ruxid2@illinois.edu>
2025-02-18 10:29:50 +0800

    logistic.py completed

--- assignment1/models/logistic.py
+++ assignment1/models/logistic.py
@@ -28,16 +28,19 @@ class Logistic:
         # TODO: implement me
         # Hint: To prevent numerical overflow, try computing the sigmoid for positive numbers and negative numbers separately.
         #       - For negative numbers, try an alternative formulation of the sigmoid function.
-        return
+
+        #return -1/(1+np.exp(z))+1
+        return np.where(z<=-7,-1/(1+np.exp(z))+1,1/(np.exp(-z)+1))
+
 
     def train(self, X_train: np.ndarray, y_train: np.ndarray):
         """Train the classifier.
 
         - Use the logistic regression update rule as introduced in lecture.
         - Initialize self.w as a matrix with random values sampled uniformly from [-1, 1)
-        and scaled by 0.01. 
+        and scaled by 0.01.
         - This initialization prevents the weights from starting too large,
-        which can cause saturation of the sigmoid function 
+        which can cause saturation of the sigmoid function
 
         Parameters:
             X_train: a numpy array of shape (N, D) containing training data;
@@ -45,7 +48,24 @@ class Logistic:
             y_train: a numpy array of shape (N,) containing training labels
         """
         # TODO: implement me
-        pass
+        self.w=0.0004*np.random.uniform(-25,25,size=(np.shape(X_train)[1],))
+
+        turn_counter=0
+        decay_it=1
+
+        while turn_counter<self.epochs:
+            train_value=X_train@self.w
+            train_log=self.sigmoid(train_value)
+
+            decay_it=0.99*decay_it
+            turn_counter=1+turn_counter
+
+            diff=(train_log-y_train)
+            gradi=(X_train.T)@diff/np.shape(X_train)[0]
+
+            self.w=self.w-gradi*decay_it*self.lr
+
+
 
     def predict(self, X_test: np.ndarray) -> np.ndarray:
         """Use the trained weights to predict labels for test data points.
@@ -60,4 +80,20 @@ class Logistic:
                 class.
         """
         # TODO: implement me
-        return
+
+        counting=0
+        predi_result=np.full(np.shape(X_test)[0],53)
+
+        trained_w=self.w
+        predi_val=X_test@trained_w
+        predi_log=self.sigmoid(predi_val)
+
+
+        while np.shape(X_test)[0]>counting:
+            if predi_log[counting]<self.threshold:
+                predi_result[counting]=0
+            else:
+                predi_result[counting]=1
+            counting=1+counting
+
+        return predi_result
